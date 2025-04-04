c8ece105969cf3f388f31f4468969a8243e743a2
r0bBy001 <robby863401@gmail.com>
2025-04-04 13:49:33 -0500

    Implementación de CRUD para TipoActoLiturgico

---
+++ liturgia-backend/src/main/java/com/example/demo/repository/TipoActoLiturgicoRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.TipoActoLiturgico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoActoLiturgicoRepository extends JpaRepository<TipoActoLiturgico, Long>{    
+
+}
